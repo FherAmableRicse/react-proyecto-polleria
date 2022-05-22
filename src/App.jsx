3e2b754550797338cf0e6bac810ce95687482b86
FherAmableRicse <amablericsefher@gmail.com>
2022-05-21 20:06:24 -0500

    archivos .env agregados

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
+import ReservasForm from './components/reservas/ReservasForm';
 import Header from './components/sections/Header';
+
 
 
 function App() {
@@ -7,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-
+      <ReservasForm />
     </div>
   );
 }
