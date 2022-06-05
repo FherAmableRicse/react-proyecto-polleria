94fb25017b909e8f0d9b249fa31793dea18ebbaa
Racha777 <jramoschang@gmail.com>
2022-06-05 16:00:19 -0500

    components pedidos PlatosCard

--- src/components/pedidos/BuscadorPlatos.jsx
+++ src/components/pedidos/BuscadorPlatos.jsx
@@ -1,27 +1,10 @@
 import usePolleria from "../../hooks/usePolleria";
+import PlatosCard from "./PlatosCard";
 const BuscadorPlatos = () => {
   const {platos}=usePolleria();
-  console.log(platos);
-
-//   const getPlatos = async () => {
-//     try {
-//       const options = {
-//         method: "GET",
-//         // url: `${process.env.REACT_APP_URL_PLATOS_JSON_URL}`,
-//         url: `https://fheramablericse.github.io/api-test/db.json`,
-//       };
-//       const { data } = await axios(options);
-//       platos=data;
-//       console.log(platos);
-//     } catch (error) {
-//       console.log(error.response.data.message);
-//     } finally {
-//         //mostrat platos
-//     }
-//   };
 
   return (
-    <div className="my-8 col-sm-8">
+    <section className="my-8 col-sm-8">
       <div>
         <h2>Carta</h2>
       </div>
@@ -33,9 +16,17 @@ const BuscadorPlatos = () => {
           id="buscadorInput"
           required
         />
-        <div className="buscador__resultados" id="buscadorResultados"></div>
+        <div className="buscador__resultados">
+          {
+            platos.map((plato)=>{
+              return(
+                <PlatosCard key={plato.id} plato={plato}/>
+              );
+            })
+          }
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
