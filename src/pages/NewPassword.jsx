311085cb418683bb136231560292c3bae756aaa9
FherAmableRicse <amablericsefher@gmail.com>
2022-08-27 12:00:07 -0500

    olvidaste password

--- src/pages/NewPassword.jsx
+++ src/pages/NewPassword.jsx
@@ -1,16 +1,12 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import Swal from 'sweetalert2';
 
 const NewPassword = () => {
-    const params = useParams();
-   // const { id } = params;
-   const id="123"
-    //console.log(params)
-    //console.log(id)
+    const navigate = useNavigate();
+    const {id} = useParams();
     
-    //const [validToken, setValidToken] = useState(false);
     const [passwordChanged, setPasswordChanged] = useState(false);
     const [passwords, setPasswords] = useState({
         password: '',
@@ -24,66 +20,63 @@ const NewPassword = () => {
           [e.target.name]: e.target.value
         });
       };
+
+
     const handleSubtmit = async (e) => {
-    e.preventDefault();
-    if ([password, repeatedPassword].includes('')) {
-        Swal.fire({
-            position: 'top',
-            icon: 'error',
-            title: 'Todos los campos son obligatorios',
-            showConfirmButton: false,
-            timer: 1500
-        })
-    } else if (password !== repeatedPassword) {
-        Swal.fire({
-            position: 'top',
-            icon: 'warning',
-            title: 'Las contraseñas no coinciden',
-            showConfirmButton: false,
-            timer: 1500
-        })
-    } else if (password.length < 6) {
-        Swal.fire({
-            position: 'top',
-            icon: 'warning',
-            title: 'El password debe contener al menos 6 caracteres',
-            showConfirmButton: false,
-            timer: 1500
-        })
-    } else {
-        try {
-        const urlNewPassword = `http://localhost:5000/usuario/new-password/${id}`;
-        await axios.post(urlNewPassword,{password:password})
-        
-        setPasswords({
-            password: '',
-            repeatedPassword: ''
-        });
-        //console.log(data.message)
-        // setAlert({
-        //     message: data.message,
-        //     error: false
-        // });
-        setPasswordChanged(true);
-        } catch (error) {
+        e.preventDefault();
+       
+        if ([password, repeatedPassword].includes('')) {
             Swal.fire({
                 position: 'top',
                 icon: 'error',
-                title: error.response.data.message,
+                title: 'Todos los campos son obligatorios',
                 showConfirmButton: false,
                 timer: 1500
             })
-            setPasswords({
-                password: '',
-                repeatedPassword: ''
-            });
+        } else if (password !== repeatedPassword) {
+            Swal.fire({
+                position: 'top',
+                icon: 'warning',
+                title: 'Las contraseñas no coinciden',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        } else if (password.length < 6) {
+            Swal.fire({
+                position: 'top',
+                icon: 'warning',
+                title: 'La contraseña debe contener al menos 6 caracteres',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        } else {
+            try {
+                    const urlNewPassword = `http://localhost:5000/usuario/new-password/${id}`;
+                
+                    await axios.post(urlNewPassword,{password:password})
+                                .then(response=>{
+                                    Swal.fire({
+                                        position: 'top',
+                                        icon: 'success',
+                                        title: response.data.message,
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    })
+                                })
+                    
+                    setPasswords({
+                        password: '',
+                        repeatedPassword: ''
+                    });
 
-        // setAlert({
-        //     message: error.response.data.message,
-        //     error: true
-        // });
+                    setPasswordChanged(true);
+
+               
+            } catch (error) {
+               console.log(error.response.data.message);
+               navigate('/login')
+            }
         }
-    }
     };
     return(
     <>
@@ -100,53 +93,53 @@ const NewPassword = () => {
             className="logo"
           />
           </div>
-              <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked/><label htmlFor="tab-1" className="tab"></label>
-              <input id="tab-2" type="radio" name="tab" className="sign-up"/><label htmlFor="tab-2" className="tab">Nueva Contraseña</label>
-              <div className="login-space">
-             
-                  <div className="login">
-                      <div className="group">
-                          <label className="label">Contraseña</label>
-                          <input 
-                          type="password" 
-                          className="input" 
-                          data-type="password" 
-                          placeholder="Nueva contraseña"
-                          name="password"
-                          value={password}
-                          onChange={handleChange}
-                          required/>
+              <form onSubmit={handleSubtmit}>
+                  <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked/><label htmlFor="tab-1" className="tab"></label>
+                  <input id="tab-2" type="radio" name="tab" className="sign-up"/><label htmlFor="tab-2" className="tab">Nueva Contraseña</label>
+                  <div className="login-space">
+                      <div className="login">
+                          <div className="group">
+                              <label className="label">Contraseña</label>
+                              <input
+                              type="password"
+                              className="input"
+                              data-type="password"
+                              placeholder="Nueva contraseña"
+                              name="password"
+                              value={password}
+                              onChange={handleChange}
+                              required/>
+                          </div>
+                          <div className="group">
+                              <label className="label">Repetir contraseña</label>
+                              <input
+                              type="password"
+                              className="input"
+                              data-type="password"
+                              placeholder="Repite tu contraseña"
+                              name="repeatedPassword"
+                              value={repeatedPassword}
+                              onChange={handleChange}
+                              required/>
+                          </div>
+                          <div className="group">
+                              <input type="submit" className="button" value="Reestablecer"
+                           
+                              />
+                          </div>
+                          {
+                            passwordChanged
+                            &&
+                            <Link
+                            to="/login"
+                            className="button"
+                            >
+                            Iniciar Sesión
+                            </Link>
+                          }
                       </div>
-                      <div className="group">
-                          <label className="label">Repetir contraseña</label>
-                          <input 
-                          type="password" 
-                          className="input" 
-                          data-type="password" 
-                          placeholder="Repite tu contraseña" 
-                          name="repeatedPassword"
-                          value={repeatedPassword}
-                          onChange={handleChange}
-                          required/>
-                      </div>
-                      <div className="group">
-                          <input type="submit" className="button" value="Reestablecer" 
-                          onClick={handleSubtmit}
-                          />
-                      </div>
-
-                      {
-                        passwordChanged
-                        && 
-                        <Link
-                        to="/login"
-                        className="button"
-                        >
-                        Iniciar Sesión
-                        </Link>
-                      }
                   </div>
-              </div>
+              </form>
           </div>
       </div>   
       </div>
