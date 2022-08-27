22d7f8935652da2d7d6994620128750861ece657
CristopherMolina <cristopher8080@gmail.com>
2022-08-26 19:22:37 -0500

    login redirec

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, } from "react-router-dom";
 import PolleriaLayout from "./layouts/PolleriaLayout";
 import Inicio from "./pages/Inicio";
 import Nosotros from "./pages/Nosotros";
